d6d9e7967a2ad3e2cc8ec8e68942abe43e85982a
Mozuuu02 <mozammilk1203@gmail.com>
2025-09-15 16:16:16 +0530

    axios error

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,27 @@
 import axios from "axios";
 
-// change this to your backend API base URL
-const API = axios.create({ baseURL: "https://mern-todo-backend-1emj.onrender.com/api/users" });
+// users API
+const USER_API = axios.create({ baseURL: "https://mern-todo-backend-1emj.onrender.com/api/users" });
+USER_API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
 
-// attach JWT token if exists
-API.interceptors.request.use((req) => {
+// tasks API
+const TASK_API = axios.create({ baseURL: "https://mern-todo-backend-1emj.onrender.com/api/tasks" });
+TASK_API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
 
 // user APIs
-export const loginUser = (data) => API.post("/login", data);
-export const register = (data) => API.post("/register", data);
+export const loginUser = (data) => USER_API.post("/login", data);
+export const register = (data) => USER_API.post("/register", data);
 
 // task APIs
-export const getTasks = () => API.get("/tasks");
-export const createTask = (data) => API.post("/tasks", data);
-export const updateTask = (id, data) => API.put(`/tasks/${id}`, data);
-export const deleteTask = (id) => API.delete(`/tasks/${id}`);
+export const getTasks = () => TASK_API.get("/");
+export const createTask = (data) => TASK_API.post("/", data);
+export const updateTask = (id, data) => TASK_API.put(`/${id}`, data);
+export const deleteTask = (id) => TASK_API.delete(`/${id}`);
